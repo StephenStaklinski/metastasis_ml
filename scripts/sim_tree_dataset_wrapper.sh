fa5fe4a97e439843be545e41c6ce7bd87326db22
Staklinski <staklins@evolgen.cshl.edu>
2023-10-31 10:11:48 -0400

    update parallel and sim

--- scripts/sim_tree_dataset_wrapper.sh
+++ scripts/sim_tree_dataset_wrapper.sh
@@ -27,6 +27,8 @@ fi
 
 conda activate simulate
 
+echo "tree_name,l1_name,l2_name,mrca_name,l1_tissue,l2_tissue,total_nodes_leaves,total_num_leaves,total_num_nodes,proportion_leaves_l1_tissue,proportion_leaves_l2_tissue,l1_sis_tissue,l2_sis_tissue,num_nodes_prior_l1,num_nodes_prior_l2,tissues_matching,dist_l1_l2,mrca_proportion_children_root_tissue,mrca_proportion_children_l1_tissue,mrca_proportion_children_l2_tissue,mrca_tissue" > tree_dataset_${NAME}.csv
+
 j=0
 for migration_matrix in "$MIGRATION"/*; do
     for ((rep=0; rep<NUM_TREES; rep++))   ### Can set the amount of repeat simulations with the same parameters
@@ -34,50 +36,23 @@ for migration_matrix in "$MIGRATION"/*; do
     output_name="batch_sim${j}"
     tree_size=100
     cmd="python scripts/simulator.py ${tree_size} ${migration_matrix} ${output_name}"
-    commands+=("$cmd")
+    data_cmd="tail -n +2 $output_name >> "tree_dataset_${NAME}.csv""
+    clean_cmd="rm ${output_name}"
+    commands+=("$cmd && $data_cmd && $clean_cmd")
     j=$((j+1))
     done
 done
 
 echo "There are ${#commands[@]} commands to be submitted."
 
-echo "tree_name,l1_name,l2_name,mrca_name,l1_tissue,l2_tissue,total_nodes_leaves,total_num_leaves,total_num_nodes,proportion_leaves_l1_tissue,proportion_leaves_l2_tissue,l1_sis_tissue,l2_sis_tissue,num_nodes_prior_l1,num_nodes_prior_l2,tissues_matching,dist_l1_l2,mrca_proportion_children_root_tissue,mrca_proportion_children_l1_tissue,mrca_proportion_children_l2_tissue,mrca_tissue" > tree_dataset_${NAME}.csv
-
-batch_size=${CORES}
-num_batches=$(((${#commands[@]} / $batch_size) + 1))
-
-echo "Beginning submission of all combinations in parallel batches of ${batch_size}..."
-for ((i=0; i<$num_batches; i++))
+for command in "${commands[@]}"
 do
-  echo "Submitting batch $((i+1))/$((num_batches)):"
-  # Calculate the range of commands to submit in this batch
-  start_index=$((i * batch_size))
-  end_index=$((start_index + batch_size - 1))
-
-  # Check if the end index is greater than or equal to the length of the commands array
-  if [ $end_index -ge ${#commands[@]} ]; then
-    batch_commands=("${commands[@]:${start_index}}") # Slice from start index to end of commands array
-  else
-    batch_commands=("${commands[@]:${start_index}:${batch_size}}") # Slice from start index to batch_size
-  fi
-
-  for command in "${batch_commands[@]}"
-  do
-    echo "${command}" >> "${i}.cmd"
-    echo "${command}"
-  done
-  # Print the ParaFly command to the console
-  ParaFly -CPU ${batch_size} -c ${i}.cmd
-  rm ${i}.cmd
-  rm ${i}.cmd.completed
-  # for f in batch_sim*; do tail -n +2 "$f" | cut -f3- -d"," >> "tree_dataset_${NAME}.csv"; done
-    for f in batch_sim*; do tail -n +2 "$f" >> "tree_dataset_${NAME}.csv"; done
-
-
-  rm batch_sim*
+  echo "${command}" >> "parallel.txt"
 done
 
-echo "All batches are done."
+parallel -j 40 < "parallel.txt"
+rm parallel.txt
+
 
 conda deactivate
 
